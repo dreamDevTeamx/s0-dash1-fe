22b7e305002eec6f6664b081c29feaa3528cf1d8
aleksandrserdyuk <av.serdiuk@yandex.by>
2022-01-22 15:14:33 +0300

    added application structure + bootstrap

---
+++ src/Components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardTable from "./DashboardTable";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <DashboardTable/>
+    </div>
+  )
+}
+
+export default Dashboard;
---
+++ src/Components/DashboardTable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashboardTable() {
+  return (
+    <div>
+      Dashboard table
+    </div>
+  );
+}
+
+export default DashboardTable;
